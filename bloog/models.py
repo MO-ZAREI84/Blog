d792c5f4c76caf21a72c75eeef75b3ca4dd589c7
MO-ZAREI84 <zareisanim@gmail.com>
2024-09-30 21:43:31 +0330

    Blog

---
+++ bloog/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Post(models.Model):
+    # STATUS_CHOISES[
+    #    ( 'DF' ,'DRAFT'),
+    #    ('PB', 'PUBLISHED'),
+    #    ('RG' , 'REGECTED'),
+    # ]
+    class Status(models.TextChoices):
+        DRAFT='DR','DRAFT'
+        PUBLISHED='PB','PUBLISHED'
+        REGECTED='RG','REGECTED'
+
+
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    slug = models.SlugField(max_length=100)
+    # CHOISEFIELD
+    status=models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
+    # relations
+    author=models.ForeignKey(User, on_delete=models.CASCADE,related_name="user_post")
+    #date
+    publish = models.DateTimeField(default=timezone.now)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-publish']
+        indexes=[
+            models.Index(fields=['-publish'])
+
+        ]
+
+
+    #over_ride
+    def __str__(self):
+        return self.title
+
