0fdaf54026b8c5b3d9199b4c3af9e42394b31011
MO-ZAREI84 <zareisanim@gmail.com>
2024-10-03 18:11:53 +0330

    11

--- bloog/admin.py
+++ bloog/admin.py
@@ -4,4 +4,12 @@ from .models  import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','publish','status']
+    ordering = ['title']
+    list_filter = ['status','author','publish']
+    search_fields = ['title','description']
+    prepopulated_fields={ "slug" : ['title'] }
+    list_editable=['status']
+    list_display_links=['title','author']
